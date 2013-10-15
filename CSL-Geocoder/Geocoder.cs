d08eb51df9f29fb661e80f5cf944131d00e136a3
Aaron Rowe <arowe4@slu.edu>
2013-10-14 22:57:50 -0500

    Initial Commit

---
+++ CSL-Geocoder/Geocoder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSL_Geocoder
+{
+    public interface Geocoder
+    {
+        //function taking json and outputting json?
+
+        //function taking xml and outputting xml?
+
+    }
+}
---
+++ CSL-Geocoder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSL_Geocoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* connect to db. make sure to use 'using' to automatically dispose of connections */
+
+            /* check for entries younger than limit */
+
+            /* pull entries and create parsable file (xml or json) */
+
+            /* geocode addresses
+             * think about letting user choose type of coder in config file
+             * all coders implement Geocoder interface */
+            Geocoder geo = new MapQuestGeocoder();
+
+            /* load coded addresses into db */
+
+            System.Console.WriteLine("Geocoding bitch!!");
+            System.Console.ReadLine();
+        }
+    }
+}
